0ed6dc4ca417886f2e7e233aece5438ee344d8f5
Khan, Abdullah <abdullah.khan@ideascale.com>
2025-09-03 19:16:10 +0600

    first commit

---
+++ src/test/java/com/cm/nativeconfigserver/NativeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.nativeconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeConfigServerApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
